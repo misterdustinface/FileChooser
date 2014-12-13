6c57d345407197f6ba9fdd5d8b0ad3eb09f46efb
Dustin Shaffer <dustin.n.shaffer@gmail.com>
2014-12-13 15:08:03 -0800

    Introduced multithreading as a method of separation of functionality.

--- FileChooser/src/FileChooserProgram/main/AWTFileChooserProgram.java
+++ FileChooser/src/FileChooserProgram/main/AWTFileChooserProgram.java
@@ -4,9 +4,11 @@ package FileChooserProgram.main;
 import AWT.UI.AWTDefaultMouseUserDevice;
 import AWT.UI.AWTEditorPanel;
 import AWT.UI.AWTFileChooser;
+import AWT.UI.AWTLayerManager;
 import AWT.UI.AWTMouseUserDevice;
 import AWT.UI.AWTProgramWindow;
 import AWT.UI.AWTSimpleUserDeviceDisplayLayer;
+import AWT.update.AWTProgramMain;
 
 public class AWTFileChooserProgram {
 	
@@ -17,9 +19,18 @@ public class AWTFileChooserProgram {
 		AWTFileChooser 		fileBrowser = new AWTFileChooser();
 		AWTMouseUserDevice 	userDevice 	= new AWTDefaultMouseUserDevice();
 		AWTEditorPanel 		editorPanel = new AWTEditorPanel(userDevice);
-
-		editorPanel.addLayer(fileBrowser);
-		editorPanel.addLayer(new AWTSimpleUserDeviceDisplayLayer(userDevice));
+		AWTLayerManager		layerManager = new AWTLayerManager();
+		layerManager.addLayer(fileBrowser);
+		layerManager.addLayer(new AWTSimpleUserDeviceDisplayLayer(userDevice));
+		
+		editorPanel.setLayerManager(layerManager);
+		
+		AWTProgramMain main = new AWTProgramMain();
+		main.setMouse(userDevice);
+		main.setLayerManager(layerManager);
+		Thread mainThread = new Thread(main);
+		mainThread.start();
+		
 		window.add(editorPanel);
 		window.revalidate();
 
