a1b6c12f9042c4a81a3e57d898687e9b313edcb0
Dustin Shaffer <dustin.n.shaffer@gmail.com>
2015-04-04 18:34:45 -0700

    ??

---
+++ src/FileChooserProgram/main/AWTFileChooserProgram.java
@@ -0,0 +1,38 @@
+package FileChooserProgram.main;
+
+
+import main.EditorProgramMain;
+import AWT.UI.AWTEditorPanel;
+import AWT.UI.AWTFileChooser;
+import AWT.UI.AWTProgramWindow;
+import AWT.UI.Mouse.AWTMouseUserDevice;
+import AWT.UI.Mouse.AWTMouseUserDeviceDisplayLayer;
+import UI.UILayerManager;
+import base.Application;
+
+public class AWTFileChooserProgram {
+	
+	public static void main(String[] args) {
+		AWTProgramWindow window = new AWTProgramWindow("File Chooser");
+		window.setSize(600, 400);
+
+		AWTFileChooser 		fileBrowser = new AWTFileChooser();
+		AWTMouseUserDevice 	userDevice 	= new AWTMouseUserDevice();
+		AWTEditorPanel 		editorPanel = new AWTEditorPanel(userDevice);
+		UILayerManager		layerManager = new UILayerManager();
+		layerManager.addLayer(fileBrowser);
+		layerManager.addLayer(new AWTMouseUserDeviceDisplayLayer(userDevice));
+		
+		editorPanel.setLayerManager(layerManager);
+		window.add(editorPanel);
+		window.revalidate();
+		
+		// STARTS FILEBROWSER //
+		fileBrowser.chooseFile();
+		
+		Application editorProgram = new Application();
+		editorProgram.setMain(EditorProgramMain.create(layerManager, userDevice));
+		editorProgram.start();
+	}
+
+}
